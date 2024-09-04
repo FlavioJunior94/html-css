4b4e9a951f0d8d0d07b3981ddebd93ffb55f10b4
Flavio Santos <flavio.santos@adapter.domain>
2024-09-04 17:08:53 -0300

    first commit

---
+++ MODULO 4/EX025/scrip.js
@@ -0,0 +1,14 @@
+const nome = document.querySelector('#inome').value
+const media = document.querySelector('#imedia').value
+const mes = document.querySelector('#imes').value
+const dia = document.querySelector('#idia').value
+const hora = document.querySelector('#ihora').value
+
+console.log(`
+nome: ${typeof(nome)} ${nome}
+media: ${typeof(media)} ${media}
+mes: ${typeof(mes)} ${mes}
+dia: ${typeof(dia)} ${dia}
+hora: ${typeof(hora)} ${hora}
+
+`);
